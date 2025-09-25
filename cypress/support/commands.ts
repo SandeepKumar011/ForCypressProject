d5ddedf2315e4f62a15e0b95d382f85422ada3ef
sonasandeep20 <automationsandeep21@gmail.com>
2025-09-25 20:30:34 +0530

    commands and customs commands added

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,9 +1,40 @@
-Cypress.Commands.add('typeInCkEditor', (text: string) => {
-  cy.get('div.ck-editor__editable[contenteditable="true"]')
-    .click()
-    .then($div => {
-      $div[0].innerHTML = `<p>${text}</p>`;
-      $div[0].dispatchEvent(new Event('input', { bubbles: true }));
-      $div[0].dispatchEvent(new Event('change', { bubbles: true }));
-    });
-});
+/// <reference types="cypress" />
+
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      /**
+       * Custom command to login to the application
+       * @example cy.login('username', 'password')
+       */
+      login(username: string, password: string): Chainable<Element>;
+      
+      /**
+       * Custom command to logout from the application
+       * @example cy.logout()
+       */
+      logout(): Chainable<Element>;
+      
+      /**
+       * Custom command to wait for page load
+       * @example cy.waitForPageLoad()
+       */
+      waitForPageLoad(): Chainable<Element>;
+      
+      /**
+       * Custom command to check if element is visible
+       * @example cy.isVisible('selector')
+       */
+      isVisible(selector: string): Chainable<Element>;
+      
+      /**
+       * Custom command to check if element is not visible
+       * @example cy.isNotVisible('selector')
+       */
+      isNotVisible(selector: string): Chainable<Element>;
+    }
+  }
+}
+
+// Export to make it a module
+export {};
